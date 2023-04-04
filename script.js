27fd132c2df5f87876a2ac1c00554c429b2c9bb4
Johan Nyman <johan.nyman97@gmail.com>
2023-04-04 20:11:54 +0200

    hidden/active toggle function

---
+++ script.js
@@ -0,0 +1,25 @@
+const signIn = document.getElementById("sign-in");
+const signInBtn = document.getElementById("sign-in-btn");
+const register = document.getElementById("register");
+const registerBtn = document.getElementById("register-btn");
+
+const displayVarations = [
+	{ active: true, display: "none" },
+	{ active: false, display: "grid" },
+];
+
+function toggleForms(clicked) {
+	console.log(clicked.id);
+	if (clicked.id == signInBtn.id) {
+		signIn.classList.replace("hidden", "active");
+		register.classList.replace("active", "hidden");
+		registerBtn.setAttribute("data", "off");
+		signInBtn.setAttribute("data", "focus");
+	}
+	if (clicked.id == registerBtn.id) {
+		signIn.classList.replace("active", "hidden");
+		register.classList.replace("hidden", "active");
+		registerBtn.setAttribute("data", "focus");
+		signInBtn.setAttribute("data", "off");
+	}
+}
