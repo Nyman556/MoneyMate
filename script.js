f280287076f8802243a1a8ebe3dc21a21c31d697
Johan Nyman <johan.nyman97@gmail.com>
2023-04-06 23:42:56 +0200

    removed credit-card animation

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const signIn = document.getElementById("sign-in");
 const signInBtn = document.getElementById("sign-in-btn");
 const register = document.getElementById("register");
 const registerBtn = document.getElementById("register-btn");
-const formContainer = document.getElementById("moveable");
+const formContainer = document.getElementById("slider");
 
 function toggleForms(clicked) {
 	console.log(clicked.id);
@@ -11,13 +11,13 @@ function toggleForms(clicked) {
 		register.classList.replace("active", "hidden");
 		registerBtn.setAttribute("data", "off");
 		signInBtn.setAttribute("data", "focus");
-		formContainer.classList = "left-side-container left";
+		formContainer.classList = "left-side-container animated left";
 	}
 	if (clicked.id == registerBtn.id) {
 		signIn.classList.replace("active", "hidden");
 		register.classList.replace("hidden", "active");
 		registerBtn.setAttribute("data", "focus");
 		signInBtn.setAttribute("data", "off");
-		formContainer.classList = "left-side-container right";
+		formContainer.classList = "left-side-container animated right";
 	}
 }
